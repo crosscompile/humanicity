b7d8c8e699757b78586d2211faafc34d1492a289
jack <jackjocross@gmail.com>
2018-10-01 22:53:56 -0400

    Add structured data

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { css } from 'glamor'
 import Img from 'gatsby-image'
 import Vivus from 'vivus'
 import { Formik, Form, Field } from 'formik'
+import { Helmet } from 'react-helmet'
 import { Trail, Spring, animated } from 'react-spring'
 import { HumanicityLogoMaroon } from '../components/Logos/Maroon'
 
@@ -22,6 +23,13 @@ const encode = data => {
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&')
 }
+
+const structuredData = JSON.stringify({
+  '@context': 'http://schema.org',
+  '@type': 'Organization',
+  name: 'Humanicity',
+  url: 'http://www.humanicity.net',
+})
 
 const labelStyle = {
   color: '#a11d4c',
@@ -89,232 +97,260 @@ class IndexPage extends React.Component {
     } = this.state
 
     return (
-      <div
-        css={{
-          position: 'relative',
-          minHeight: '100%',
-          fontWeight: 200,
-        }}
-      >
+      <>
+        <Helmet
+          title="Humanicity"
+          meta={[
+            {
+              property: 'og:title',
+              content: 'Humanicity',
+            },
+            {
+              property: 'og:description',
+              content: 'Because customers are people first.',
+            },
+            {
+              property: 'description',
+              content: 'Because customers are people first.',
+            },
+          ]}
+        >
+          <script type="application/ld+json">{structuredData}</script>
+        </Helmet>
         <div
           css={{
-            width: '100%',
-            maxWidth: 700,
-            padding: '5rem 0 0 0',
-            margin: '0 auto',
-            color: 'white',
+            position: 'relative',
+            minHeight: '100%',
+            fontWeight: 200,
           }}
         >
-          <HumanicityLogoMaroon
-            id="logo"
-            role="img"
+          <div
             css={{
-              ...(showLogo ? {} : { opacity: 0 }),
               width: '100%',
-              height: 'auto',
-              transition: 'all 1s',
+              maxWidth: 700,
+              padding: '5rem 0 0 0',
+              margin: '0 auto',
+              color: 'white',
             }}
-            fillOpacity={logoAnimated ? 1 : 0}
-            strokeWidth={logoAnimated ? 0 : 2}
-            stroke="#a11d4c"
-          />
-        </div>
-        <div
-          css={{
-            width: '100%',
-            maxWidth: 555,
-            margin: '0 auto',
-            padding: '0 1rem 5rem 1rem',
-            color: 'white',
-          }}
-        >
-          {!contactSubmitted ? (
-            <>
+          >
+            <HumanicityLogoMaroon
+              id="logo"
+              role="img"
+              css={{
+                ...(showLogo ? {} : { opacity: 0 }),
+                width: '100%',
+                height: 'auto',
+                transition: 'all 1s',
+              }}
+              fillOpacity={logoAnimated ? 1 : 0}
+              strokeWidth={logoAnimated ? 0 : 2}
+              stroke="#a11d4c"
+            />
+          </div>
+          <div
+            css={{
+              width: '100%',
+              maxWidth: 555,
+              margin: '0 auto',
+              padding: '0 1rem 5rem 1rem',
+              color: 'white',
+            }}
+          >
+            {!contactSubmitted ? (
+              <>
+                <Spring
+                  native
+                  from={{ opacity: 0, transform: 'translateY(20px)' }}
+                  to={{
+                    opacity: logoAnimated ? 1 : 0,
+                    transform: logoAnimated
+                      ? 'translateY(0px)'
+                      : 'translateY(20px)',
+                  }}
+                  onRest={this.handleRest}
+                >
+                  {style => (
+                    <animated.p
+                      style={style}
+                      css={{
+                        color: '#a11d4c',
+                        textAlign: 'center',
+                        margin: '0 auto 5rem auto',
+                        fontSize: '1.8rem',
+                        '@media(min-width: 555px)': {
+                          fontSize: '2.3rem',
+                        },
+                      }}
+                    >
+                      Because customers are people first.
+                    </animated.p>
+                  )}
+                </Spring>
+                <Trail
+                  native
+                  from={{ opacity: 0, transform: 'translateY(20px)' }}
+                  to={{
+                    opacity: taglineAnimated ? 1 : 0,
+                    transform: taglineAnimated
+                      ? 'translateY(0px)'
+                      : 'translateY(20px)',
+                  }}
+                  keys={['tagline', 'directions', 'form']}
+                >
+                  {style => (
+                    <animated.p
+                      style={style}
+                      css={{
+                        fontSize: '1.1rem',
+                        marginBottom: '3rem',
+                        lineHeight: '1.5rem',
+                      }}
+                    >
+                      While we put the finishing touches on our website, send us
+                      a hello to learn more about our Human Immersion branding
+                      approach.
+                    </animated.p>
+                  )}
+                  {style => (
+                    <animated.div style={style}>
+                      <Formik
+                        initialValues={{ name: '', email: '', message: '' }}
+                        onSubmit={this.handleSubmit}
+                      >
+                        <Form
+                          name="contact"
+                          method="post"
+                          data-netlify="true"
+                          data-netlify-honeypot="bot-field"
+                        >
+                          <input
+                            type="hidden"
+                            name="form-name"
+                            value="contact"
+                          />
+                          <Field
+                            id="name"
+                            name="name"
+                            render={({ field }) => (
+                              <p css={{ marginBottom: '2rem' }}>
+                                <label htmlFor="name" css={labelStyle}>
+                                  Name
+                                </label>
+                                <input
+                                  type="text"
+                                  required={true}
+                                  css={inputStyle}
+                                  {...field}
+                                />
+                              </p>
+                            )}
+                          />
+
+                          <Field
+                            id="email"
+                            name="email"
+                            render={({ field, form }) => (
+                              <p css={{ marginBottom: '2rem' }}>
+                                <label htmlFor="email" css={labelStyle}>
+                                  Email
+                                </label>
+                                <input
+                                  type="email"
+                                  required={true}
+                                  css={inputStyle}
+                                  {...field}
+                                />
+                              </p>
+                            )}
+                          />
+
+                          <Field
+                            id="message"
+                            name="message"
+                            render={({ field }) => (
+                              <p css={{ marginBottom: '2rem' }}>
+                                <label htmlFor="message" css={labelStyle}>
+                                  Message
+                                </label>
+                                <textarea css={inputStyle} {...field} />
+                              </p>
+                            )}
+                          />
+                          <div
+                            css={{
+                              display: 'flex',
+                              justifyContent: 'flex-end',
+                            }}
+                          >
+                            <button
+                              type="submit"
+                              css={{
+                                background: '#a11d4c',
+                                padding: '.6rem 1.5rem',
+                                color: 'white',
+                                cursor: 'pointer',
+                                border: '1px solid #a11d4c',
+                                borderRadius: 3,
+                                transition: 'all 100ms ease-in',
+                              }}
+                            >
+                              Submit
+                            </button>
+                          </div>
+                        </Form>
+                      </Formik>
+                    </animated.div>
+                  )}
+                </Trail>
+              </>
+            ) : (
               <Spring
                 native
                 from={{ opacity: 0, transform: 'translateY(20px)' }}
-                to={{
-                  opacity: logoAnimated ? 1 : 0,
-                  transform: logoAnimated
-                    ? 'translateY(0px)'
-                    : 'translateY(20px)',
-                }}
-                onRest={this.handleRest}
+                to={{ opacity: 1, transform: 'translateY(0px)' }}
               >
-                {style => (
+                {styles => (
                   <animated.p
-                    style={style}
+                    style={styles}
                     css={{
-                      color: '#a11d4c',
                       textAlign: 'center',
-                      margin: '0 auto 5rem auto',
-                      fontSize: '1.8rem',
-                      '@media(min-width: 555px)': {
-                        fontSize: '2.3rem',
-                      },
+                      color: '#a11d4c',
+                      fontSize: '2rem',
+                      margin: '3rem 0',
                     }}
                   >
-                    Because customers are people first.
+                    Thank you! We will be in touch.
                   </animated.p>
                 )}
               </Spring>
-              <Trail
-                native
-                from={{ opacity: 0, transform: 'translateY(20px)' }}
-                to={{
-                  opacity: taglineAnimated ? 1 : 0,
-                  transform: taglineAnimated
-                    ? 'translateY(0px)'
-                    : 'translateY(20px)',
-                }}
-                keys={['tagline', 'directions', 'form']}
-              >
-                {style => (
-                  <animated.p
-                    style={style}
-                    css={{
-                      fontSize: '1.1rem',
-                      marginBottom: '3rem',
-                      lineHeight: '1.5rem',
-                    }}
-                  >
-                    While we put the finishing touches on our website, send us a
-                    hello to learn more about our Human Immersion branding
-                    approach.
-                  </animated.p>
-                )}
-                {style => (
-                  <animated.div style={style}>
-                    <Formik
-                      initialValues={{ name: '', email: '', message: '' }}
-                      onSubmit={this.handleSubmit}
-                    >
-                      <Form
-                        name="contact"
-                        method="post"
-                        data-netlify="true"
-                        data-netlify-honeypot="bot-field"
-                      >
-                        <input type="hidden" name="form-name" value="contact" />
-                        <Field
-                          id="name"
-                          name="name"
-                          render={({ field }) => (
-                            <p css={{ marginBottom: '2rem' }}>
-                              <label htmlFor="name" css={labelStyle}>
-                                Name
-                              </label>
-                              <input
-                                type="text"
-                                required={true}
-                                css={inputStyle}
-                                {...field}
-                              />
-                            </p>
-                          )}
-                        />
-
-                        <Field
-                          id="email"
-                          name="email"
-                          render={({ field, form }) => (
-                            <p css={{ marginBottom: '2rem' }}>
-                              <label htmlFor="email" css={labelStyle}>
-                                Email
-                              </label>
-                              <input
-                                type="email"
-                                required={true}
-                                css={inputStyle}
-                                {...field}
-                              />
-                            </p>
-                          )}
-                        />
-
-                        <Field
-                          id="message"
-                          name="message"
-                          render={({ field }) => (
-                            <p css={{ marginBottom: '2rem' }}>
-                              <label htmlFor="message" css={labelStyle}>
-                                Message
-                              </label>
-                              <textarea css={inputStyle} {...field} />
-                            </p>
-                          )}
-                        />
-                        <div
-                          css={{ display: 'flex', justifyContent: 'flex-end' }}
-                        >
-                          <button
-                            type="submit"
-                            css={{
-                              background: '#a11d4c',
-                              padding: '.6rem 1.5rem',
-                              color: 'white',
-                              cursor: 'pointer',
-                              border: '1px solid #a11d4c',
-                              borderRadius: 3,
-                              transition: 'all 100ms ease-in',
-                            }}
-                          >
-                            Submit
-                          </button>
-                        </div>
-                      </Form>
-                    </Formik>
-                  </animated.div>
-                )}
-              </Trail>
-            </>
-          ) : (
-            <Spring
-              native
-              from={{ opacity: 0, transform: 'translateY(20px)' }}
-              to={{ opacity: 1, transform: 'translateY(0px)' }}
-            >
-              {styles => (
-                <animated.p
-                  style={styles}
-                  css={{
-                    textAlign: 'center',
-                    color: '#a11d4c',
-                    fontSize: '2rem',
-                    margin: '3rem 0',
-                  }}
-                >
-                  Thank you! We will be in touch.
-                </animated.p>
-              )}
-            </Spring>
-          )}
-        </div>
-        <div
-          css={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: -1,
-          }}
-        >
-          <Img
-            sizes={splash.childImageSharp.sizes}
-            alt=""
-            imgStyle={{
-              objectFit: 'cover',
-              objectPosition: '50% 50%',
-              filter: 'brightness(15%)',
-            }}
-            outerWrapperClassName={css({ height: '100%' })}
+            )}
+          </div>
+          <div
             css={{
-              height: '100%',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              zIndex: -1,
             }}
-          />
+          >
+            <Img
+              sizes={splash.childImageSharp.sizes}
+              alt=""
+              imgStyle={{
+                objectFit: 'cover',
+                objectPosition: '50% 50%',
+                filter: 'brightness(15%)',
+              }}
+              outerWrapperClassName={css({ height: '100%' })}
+              css={{
+                height: '100%',
+              }}
+            />
+          </div>
         </div>
-      </div>
+      </>
     )
   }
 }
